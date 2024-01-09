e971fb64193af5ce533b5d8b7398d33d874aaa7f
Andrei Lavrenov <andrei@lavrenov.io>
2024-01-09 12:07:04 +0100

    change deploy base for gh pages

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/adventofcode2023/',
     plugins: [react()],
 });
